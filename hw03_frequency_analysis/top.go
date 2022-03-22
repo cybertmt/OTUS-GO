ab17e3c0169a2ce1704b4b97644bd513ed3818f2
cybertmt <cyber87@yandex.ru>
2022-03-22 21:27:27 +0300

    -- optimized slice init and map inputs

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -10,36 +10,27 @@ func Top10(str string) []string {
 	if str == "" {
 		return []string{}
 	}
-
 	// Для задания со '*' очищаем текст от знаков и конвертируем заглавные буквы в строчные
 	for _, c := range []string{"!", "'", ",", ".", "- "} {
 		str = strings.ReplaceAll(str, c, " ")
 	}
 	str = strings.ToLower(str)
-
-	// Слайс слов, возвращаемый функцией Top10
-	strResult := make([]string, 0)
+	// Разбираем исходную строку на слайс слов с пробелом в качестве разделителя
+	strSlice := strings.Fields(str)
 	// Map для подсчета слов в тексте [слово]количество
 	wordMap := make(map[string]int)
+	// Наполняем map с подсчетом количества слов
+	for _, s := range strSlice {
+		wordMap[s]++
+	}
 	// Структура для подсчета количества слов,
 	// используется при сортировке по значениям полей
 	type wordStruct struct {
 		Word  string
 		Count int
 	}
-	// Разбираем исходную строку на слайс слов с пробелом в качестве разделителя
-	strSlice := strings.Fields(str)
-	// Наполняем map с подсчетом количества слов
-	for _, s := range strSlice {
-		_, ok := wordMap[s]
-		if ok {
-			wordMap[s]++
-			continue
-		}
-		wordMap[s] = 1
-	}
 	// Создаем слайс структур для слов и наполняем его из map с количеством слов
-	wordStructSlice := make([]wordStruct, 0)
+	wordStructSlice := make([]wordStruct, 0, len(wordMap))
 	for k, v := range wordMap {
 		wordStructSlice = append(wordStructSlice, wordStruct{k, v})
 	}
@@ -50,6 +41,8 @@ func Top10(str string) []string {
 		}
 		return wordStructSlice[i].Word < wordStructSlice[j].Word
 	})
+	// Слайс слов, возвращаемый функцией Top10
+	strResult := make([]string, 0, 10)
 	// Первые 10 слов отсортированного слайса добавляем в результирующий слайс и возвращаем
 	for _, v := range wordStructSlice[:10] {
 		strResult = append(strResult, v.Word)
