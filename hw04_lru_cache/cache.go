f19a08ae015a79cec6df6b2771e5131180421597
cybertmt <cyber87@yandex.ru>
2022-04-02 13:16:53 +0300

    -- comments updated

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -23,6 +23,7 @@ type cacheItem struct {
 	value interface{}
 }
 
+// cacheItemMap дополнительный словарь [указатель на эелемент]{ключ, значение элемента}.
 var cacheItemMap = make(map[*ListItem]cacheItem)
 
 func NewCache(capacity int) Cache {
@@ -33,15 +34,21 @@ func NewCache(capacity int) Cache {
 	}
 }
 
+// Clear полностью обнуляет кэш.
 func (l *lruCache) Clear() {
 	l.queue = NewList()
 	l.items = make(map[Key]*ListItem, l.capacity)
 }
 
+// Set добавляет элемент в кэш.
 func (l *lruCache) Set(key Key, value interface{}) bool {
+	// Блокируем кэш для изменения/чтения.
 	mu.Lock()
 	defer mu.Unlock()
+	// Проверяем
 	i, ok := l.items[key]
+	// Если ключ уже присутвует в кэше, обновляем значение элемента в списке и словаре cacheItemMap .
+	// Переносим элемент в начало списка. Возвращаем true.
 	if ok {
 		l.items[key].Value = value
 		cacheItemMap[l.items[key]] = cacheItem{key, value}
@@ -53,18 +60,25 @@ func (l *lruCache) Set(key Key, value interface{}) bool {
 		delete(l.items, keyToRemove)
 		l.queue.Remove(l.queue.Back())
 	}
+	// Если ключ отсутсвует в кэше, добавляем элемент в начало списка.
+	// Добавляем элемент в словарь cacheItemMap. Возвращаем false.
 	l.items[key] = l.queue.PushFront(value)
 	cacheItemMap[l.items[key]] = cacheItem{key, value}
 	return false
 }
 
+// Get получает значение элемента из кэша.
 func (l *lruCache) Get(key Key) (interface{}, bool) {
+	// Блокируем кэш для изменения/чтения.
 	mu.Lock()
 	defer mu.Unlock()
+	// Если элемент присутвует в кэше, передвигаем элемент в начало списка.
+	// Возвращаем значение элемента и true.
 	i, ok := l.items[key]
 	if ok {
 		l.queue.MoveToFront(i)
 		return i.Value, true
 	}
+	// Если элемент отсутвует в кэше, возвращаем nil и false.
 	return nil, false
 }
